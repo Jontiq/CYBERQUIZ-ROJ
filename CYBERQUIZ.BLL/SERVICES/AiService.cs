11c5b6f91cf856373b8e9a42853c1a46e5bbbc03
Jontiq <99jonathancar@gmail.com>
2026-03-03 13:55:24 +0100

    - Skapat "GetIncorrectAnswersAsync" i ProfileService - Löst så att quiz kan "sparas" så det funkar att låsa upp nya quiz och se progress både i profilsidan och när man ser quizöversikten. - Lagt till ny metod för "GetBestScoreAsync" för att kunna presentera bättre progress när man ser över kategorivy - Ändrat lite UI

---
+++ CYBERQUIZ.BLL/SERVICES/AiService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http.Json;
+using System.Text;
+
+namespace CYBERQUIZ.BLL.SERVICES
+{
+    //I princip direktkopierad från movie api projektet vi gjorde
+    public class AiService
+    {
+
+        private readonly HttpClient _httpClient;
+
+        public AiService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+            _httpClient.BaseAddress = new Uri("http://localhost:11434"); //KONTROLLERA DENNA
+        }
+
+        public async Task<string> AskAsync(string prompt)
+        {
+            var request = new
+            {
+                model = "phi3", //Här byter man modell, men måste göra en pull först (ex. powershell)
+                prompt = prompt,
+                stream = false
+            };
+
+            var response = await _httpClient.PostAsJsonAsync("/api/generate", request);
+            var result = await response.Content.ReadFromJsonAsync<OllamaResponse>();
+
+            return result.response;
+
+        }
+
+        public class OllamaResponse
+        {
+            public string response { get; set; }
+        }
+
+    }
+}
---
+++ CYBERQUIZ.BLL/SERVICES/IAiService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CYBERQUIZ.BLL.SERVICES
+{
+    public interface IAiService
+    {
+        Task<string> AskAsync(string prompt);
+    }
+}
