c84428c96cf5051c2513307a6669bf4bc52cd107
Jontiq <99jonathancar@gmail.com>
2026-03-03 09:37:29 +0100

    - Lagt till ny sida "QuizQuestion" som ansvarar för presentationen av varje quiz - Ändrar lite logik i QuizHome för att generera kateogrier och dess länkar

--- CYBERQUIZ.UI/Pages/QuizPages/QuestionPages/QuizQuestion.cshtml.cs
+++ CYBERQUIZ.UI/Pages/QuizPages/QuestionPages/QuizQuestion.cshtml.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace CYBERQUIZ.UI.Pages.QuizPages.QuestionPages
 {
-    public class TvåfactorsautentiseringModel : PageModel
+    public class QuizQuestionModel : PageModel
     {
         public void OnGet()
         {
