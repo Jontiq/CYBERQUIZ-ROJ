00f013aeb558ba5f3a0f17896bd146a07cf24fd4
Jontiq <99jonathancar@gmail.com>
2026-03-04 11:01:16 +0100

    Lägger till AI DTO

---
+++ CYBERQUIZ.BLL/DTOS/AiDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CYBERQUIZ.BLL.DTOS
+{
+    public class AiDto
+    {
+        public string QuestionText { get; set; } = string.Empty;
+        public string AiRecommendation { get; set; } = string.Empty;
+    }
+}
